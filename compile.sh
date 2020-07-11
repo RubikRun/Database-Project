414baa3df4207ac4d82e8c0ae6c18af833c01d2d
Boris Dimitrov <rubik@192.168.1.8>
2020-07-11 10:17:10 +0300

    added a main file and a compile script file

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ main.cpp -o app Database/*.cpp String/*.cpp System/*.cpp Table/*.cpp
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "System/System.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    System system;
+    system.Run();
+    
+    return 0;
+}
